fixed the terminal prediction computation graph - hopefully gradients are flowing well now.

train.py
@@ -8,6 +8,8 @@ from utils import ensure_shared_grads, np, v_wrap
 from model import A3Clstm
 from player_util import Agent
 from torch.autograd import Variable
+from torchviz import make_dot
+
 
 
 def train(rank, args, shared_model, optimizer, env_conf):
@@ -75,6 +77,7 @@ def train(rank, args, shared_model, optimizer, env_conf):
             value, _, _, _, _ = player.model((Variable(player.state.unsqueeze(0)), (player.hx, player.cx)))
             R = value.data
 
+
         if gpu_id >= 0:
             with torch.cuda.device(gpu_id):
                 R = R.cuda()
@@ -82,12 +85,11 @@ def train(rank, args, shared_model, optimizer, env_conf):
         player.values.append(Variable(R))
         policy_loss = 0
         value_loss = 0
+        reward_pred_loss = 0
 
-        terminal_loss = torch.zeros(1,1) # added terminal loss here
-        terminal_loss = Variable(terminal_loss, requires_grad=True)
 
-        reward_pred_loss = torch.zeros(1, 1)  # added terminal loss here
-        reward_pred_loss = Variable(terminal_loss, requires_grad=True)
+        terminal_loss = 0
+
 
 
         gae = torch.zeros(1, 1)
@@ -95,7 +97,7 @@ def train(rank, args, shared_model, optimizer, env_conf):
             with torch.cuda.device(gpu_id):
                 gae = gae.cuda()
         R = Variable(R) # TODO why this is here?
-        loss_fn = torch.nn.L1Loss()
+        loss_fn = torch.nn.MSELoss()
 
         for i in reversed(range(len(player.rewards))):
             R = args.gamma * R + player.rewards[i]
@@ -108,38 +110,48 @@ def train(rank, args, shared_model, optimizer, env_conf):
 
             policy_loss = policy_loss - player.log_probs[i] * Variable(gae) - 0.01 * player.entropies[i]
 
+            if args.reward_prediction:
+                reward_pred_loss = reward_pred_loss + (player.reward_predictions[i] - player.rewards[i]).pow(2)
+
         # New part for the Reward Prediction Auxiliary Task
-        if args.reward_prediction:
-            actual_rewards = v_wrap(np.asarray(player.rewards), np.float32).unsqueeze(1)
-            predicted_rewards = Variable(torch.tensor(player.reward_predictions).unsqueeze(1), requires_grad=True)
+        #if args.reward_prediction:
+        #    actual_rewards = v_wrap(np.asarray(player.rewards), np.float32).unsqueeze(1)
+        #    predicted_rewards = Variable(torch.tensor(player.reward_predictions).unsqueeze(1), requires_grad=True)
 
-            if gpu_id >= 0:
-                with torch.cuda.device(gpu_id):
-                    reward_pred_loss = terminal_loss.cuda()
-                    predicted_rewards = predicted_rewards.cuda()
-                    actual_rewards = actual_rewards.cuda()
+        #    if gpu_id >= 0:
+        #        with torch.cuda.device(gpu_id):
+        #            predicted_rewards = predicted_rewards.cuda()
+        #            actual_rewards = actual_rewards.cuda()
 
-            reward_pred_loss = reward_pred_loss + loss_fn(predicted_rewards, actual_rewards)
+        #    reward_pred_loss = loss_fn(predicted_rewards, actual_rewards)
 
         # New Part for the Terminal Prediction Auxiliary Task
         if args.terminal_prediction and player.done: # this is the only time we can get labels
             # create the terminal self-supervised labels
-            end_predict_labels = v_wrap(np.arange(1, (len(player.terminal_predictions) + 1)) / (len(player.terminal_predictions) + 1), np.float32).unsqueeze(1)  # get torch version.
-            tensor_terminal_predictions = Variable(torch.tensor(player.terminal_predictions).unsqueeze(1), requires_grad=True)
+            end_predict_labels = np.arange(1, (len(player.terminal_predictions) + 1)) / (len(player.terminal_predictions) + 1)
 
-            if gpu_id >= 0:
-                with torch.cuda.device(gpu_id):
-                    terminal_loss = terminal_loss.cuda()
-                    tensor_terminal_predictions = tensor_terminal_predictions.cuda()
-                    end_predict_labels = end_predict_labels.cuda()
+            for i in range(len(player.terminal_predictions)):
+                 terminal_loss = terminal_loss + ( player.terminal_predictions[i] - end_predict_labels[i] ).pow(2)
 
-            terminal_loss = terminal_loss + loss_fn(tensor_terminal_predictions, end_predict_labels)
+            #print(terminal_loss)
+
             player.terminal_predictions = []  # Note that this is not done in clear_actions method as terminal labels are received at the end of episode
 
         player.model.zero_grad()
         #print(f"policy loss {policy_loss} and value loss {value_loss} and terminal loss {terminal_loss} and reward pred loss {reward_pred_loss}")
 
-        (policy_loss + 0.5 * value_loss + terminal_loss + reward_pred_loss).backward()
+        total_loss = policy_loss + 0.5 * value_loss + terminal_loss + reward_pred_loss
+
+        if args.terminal_prediction and player.done is False:
+            total_loss.backward(retain_graph=True)
+        else:
+            total_loss.backward() # will free memory ...
+
+        # Visualize Computation Graph
+        #graph = make_dot(total_loss)
+        #from graphviz import Source
+        #Source.view(graph)
+
         ensure_shared_grads(player.model, shared_model, gpu=gpu_id >= 0)
         optimizer.step()
         player.clear_actions()